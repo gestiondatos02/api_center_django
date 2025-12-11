a0cb672ebd7c39ca9d2ea364fe9e5a0ca664c7ea
DAVID ADREANI <gestion.datos02@grupo-amanecer.com>
2025-12-11 12:38:24 -0400

    [EDIT] urls conf

--- api_center/urls.py
+++ api_center/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("n8n/", include('n8n.urls')),
-    # path("dispatch_lines", include('dispatch_lines.urls')),
-    # path("odoo_budget", include('odoo_budget.urls')),
+    # path("dispatch_lines/", include('dispatch_lines.urls')),
+    # path("odoo_budgets/", include('odoo_budgets.urls')),
 ]
